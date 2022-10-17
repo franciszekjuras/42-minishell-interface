ee59c32df66bdaaa29e500a1d338a3462bfcb190
fjuras <fjuras@student.42wolfsburg.de>
2022-10-17 21:44:07 +0200

    Expanded example with simple parser

--- example.c
+++ example.c
@@ -6,11 +6,12 @@
 /*   By: fjuras <fjuras@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 20:12:03 by fjuras            #+#    #+#             */
-/*   Updated: 2022/10/13 15:52:10 by fjuras           ###   ########.fr       */
+/*   Updated: 2022/10/17 21:43:37 by fjuras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <string.h>
 #include "line.h"
 #include "test_line.h"
 
@@ -28,6 +29,57 @@ t_line	dummy_parser(const char * line_str)
 	test_prog_redirs(&line.progs[i++], NULL, "out.txt");
 	test_line_end(&line, i);
 	return (line);
+}
+
+t_line simplest_parser(char *line_str)
+{
+	t_line	line;
+
+	line.size = 1;
+	line.progs = malloc(sizeof(t_prog));
+	line.progs[0].args = calloc(2, sizeof(char *));
+	line.progs[0].args[0] = strdup(line_str);
+	line.progs[0].args[1] = NULL;
+	line.progs[0].in_redir.name = NULL;
+	line.progs[0].out_redir.name = NULL;
+	return (line);
+}
+
+
+int	test_simplest(void)
+{
+	t_line	line;
+	t_line	expect;
+	int		i;
+	int		res;
+
+	TEST_START();
+	i = 0;
+	test_line_init(&expect, 1);
+	test_prog_args(&expect.progs[i], 1, "ls");
+	test_prog_redirs(&expect.progs[i++], NULL, NULL);
+	test_line_end(&expect, i);
+	line = simplest_parser("ls");
+	res = test_line_eq_print(&line, &expect);
+	return (TEST_END(res));
+}
+
+int	test_bit_harder(void)
+{
+	t_line	line;
+	t_line	expect;
+	int		i;
+	int		res;
+
+	TEST_START();
+	i = 0;
+	test_line_init(&expect, 1);
+	test_prog_args(&expect.progs[i], 2, "grep", "total");
+	test_prog_redirs(&expect.progs[i++], NULL, NULL);
+	test_line_end(&expect, i);
+	line = simplest_parser("grep total");
+	res = test_line_eq_print(&line, &expect);
+	return (TEST_END(res));
 }
 
 int	test_dummy_passing(void)
@@ -86,11 +138,13 @@ int	test_dummy_broken(void)
 	test_prog_redirs(&expect.progs[i++], "in.txt", NULL);
 	test_line_end(&expect, i);
 	line = dummy_parser("<in.txt ls -l | grep total >out.txt");	
-	return (test_end(__func__, test_line_eq_print(&line, &expect)));
+	res = test_line_eq_print(&line, &expect);
 	return (TEST_END(res));
 }
 
 int (*const test_functions[])() = {
+	test_simplest,
+	test_bit_harder,
     test_dummy_not_passing,
     test_dummy_passing,
 	// test_dummy_broken,
@@ -116,4 +170,5 @@ int	main(void)
 		printf("    %s %d of %d tests failed\n", TEST_STR_FAIL,
 			total - passed, total);
 	printf("^^^\n");
+	return (passed < total);
 }
